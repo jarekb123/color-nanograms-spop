f5f52420b32ea66b1c2981377310610201161898
Jaroslaw Butajlo <jaroslaw.butajlo@allbright.io>
2019-05-23 21:05:43 +0200

    \#2 Fix isBlocksMatching

--- app/Main.hs
+++ app/Main.hs
@@ -34,20 +34,31 @@ main = do
   putStrLn $ "--- Empty nanogram ---"
   prettyPrint nanogram
 
-  putStrLn $ "--- Solved nanogram ---"
-  let nanogram1 = putColor nanogram Red 0 0
-  let nanogram2 = putColor (putColor (putColor nanogram1 Red 1 1) Black 0 1) Black 2 2
+  putStrLn $ "--- Testing nanogram ---"
+  let nanogram1 = putColor nanogram Black 1 0
+  let nanogram2 = putColor (putColor (putColor nanogram1 Red 1 1) Black 0 1) Orange 2 1
   prettyPrint nanogram2
 
   let row0 = getRow nanogram2 0
-  let row0Constr = columnsConstraints !! 0
+  let row0Constr = rowsConstraints !! 0
   putStrLn $ ("Row 0: " ++ show row0)
   putStrLn $ ("Row 0: isMatchingConstraints: " ++ show (isMatchingConstraints row0 row0Constr))
 
   let row1 = getRow nanogram2 1
-  let row1Constr = columnsConstraints !! 1
+  let row1Constr = rowsConstraints !! 1
   putStrLn $ ("Row 1: " ++ show row1)
   putStrLn $ ("Row 1 to blocks: " ++ show (colorsToBlocks row1))
+  putStrLn $ ("Row 1 constraints: " ++ show row1Constr)
   putStrLn $ ("Row 1: isMatchingConstraints: " ++ show (isMatchingConstraints row1 row1Constr))
+
+  let col0 = getColumn nanogram2 0
+  let col0Constr = columnsConstraints !! 0
+  putStrLn $ ("Col 0: " ++ show col0)
+
+  putStrLn $ ("Col 0: isMatchingConstraints: " ++ show (isMatchingConstraints col0 col0Constr))
+
+  putStrLn $ ("Row 0 & Col 0: isRowColMatchesConstr: " ++
+    show (isColRowMatchesConstr nanogram2 columnsConstraints rowsConstraints 0 0))
+
 
   return ()
--- src/Solver.hs
+++ src/Solver.hs
@@ -3,7 +3,7 @@ module Solver where
 import           Data.List
 import           Model
 import           Utils
-
+import Input
 
 emptyNanogram :: Int -> Int -> Nanogram
 emptyNanogram cols rows = generate (generate Empty cols) rows
@@ -24,15 +24,16 @@ putColor n color col row = replaceAt2 n color col row
 
   1. brak warunkow do sprawdzenia, a pozostaly bloki do sprawdzenia -> FALSE
   2. sa warunki do sprawdzenia, a brak pozostalych blokow do sprawdzenia -> TRUE
-  3. brak warunkow, brak blokow do sprawdzenia -> TRUE (ten warunek pokrywa sie z tym wyzej)
+  3. brak warunkow, brak blokow do sprawdzenia -> TRUE
   4. kolor bloku warunku != kolor bloku sprawdzanego -> sprawdz z nastepnym warunkiem
   5. (kolor bloku warunku == kolor bloku sprawdzanego) && (liczba zamalowanych > max wielkosc bloku) ->
       sprawdz dla nastepnego bloku warunku i aktualnego bloku sprawdzanego
   6. otherwise -> sprawdz dla nastepnego bloku warunku i nastepnego bloku sprawdzanego
 -}
 isBlocksMatching :: [Block] -> [Block] -> Bool
-isBlocksMatching [] b = False
-isBlocksMatching b [] = True
+isBlocksMatching [] [] = True
+isBlocksMatching [] (b:bs) = False
+isBlocksMatching (b:bs) [] = True
 isBlocksMatching ((n1, c1):bs1) ((n2, c2):bs2)
   | c2 /= c1 = isBlocksMatching bs1 ((n2,c2):bs2)
   | (c2 == c1) && (n2 > n1) = isBlocksMatching bs1 ((n2,c2):bs2)
@@ -46,6 +47,27 @@ isMatchingConstraints col ctr
     let colorsBlocks = colorsToBlocks col
         ctrBlocks = constraintsToBlocks ctr
     in  isBlocksMatching ctrBlocks colorsBlocks
+
+{-|
+  Funkcja sprawdzajaca czy podany kolumna i wiersz z nanogramu spelniaja warunki
+  1st arg: nanogram
+  2nd arg: columns constraints
+  3rd arg: rows constraints
+  4th arg: column number
+  5th arg: row number
+-}
+isColRowMatchesConstr :: Nanogram -> [Constraints] -> [Constraints] -> Int -> Int -> Bool
+isColRowMatchesConstr n cc rc c r =
+  let isColumnMatching n cc c =
+        let col = getColumn n c
+            colConstr = cc !! c
+        in  isMatchingConstraints col colConstr
+      isRowMatching n rc r =
+        let row = getRow n c
+            rowConstr = rc !! r
+        in  isMatchingConstraints row rowConstr
+  in (isColumnMatching n cc c) && (isRowMatching n rc r)
+
 
 ------ Mappers ----
 colorsToBlocks :: [Color] -> [Block]
