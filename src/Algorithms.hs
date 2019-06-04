eea5de5b583ea5dee7052088fdce52c369b8f49a
Jaroslaw Butajlo <jaroslaw.butajlo@allbright.io>
2019-06-04 15:26:42 +0200

    create function that generates all possible solutions of row/col

---
+++ src/Algorithms.hs
@@ -0,0 +1,56 @@
+{- Plik ten zawiera algorytm generujacy wszystkie możliwe rozwiązania danego wiersza -}
+module Algorithms where
+
+import           Data.List
+import           Model
+
+-- Funkcja zliczająca ilość pustych komórek może być jeszcze wstawionych w wierszu/kolumnie
+--
+-- 1st arg: [Block] - warunki wiersza/kolumny
+-- 2nd arg: Int - wielkosc wiersza kolumny
+countEmpty :: [Block] -> Int -> Int
+countEmpty [] n          = n
+countEmpty ((x, c):bs) n = countEmpty bs (n - x)
+
+-- Funkcja wstawiająca wymagane puste komórki w wierszu/kolumnie.
+--
+-- NOTE: Puste komórki są wymagane pomiędzy blokami tego samego koloru
+addNeededEmptyCells :: [Block] -> [Block]
+addNeededEmptyCells [] = []
+addNeededEmptyCells [x] = [x]
+addNeededEmptyCells ((x1, c1):(x2, c2):xs)
+  | c1 == c2 = (x1, c1) : (1, Empty) : (x2, c2) : addNeededEmptyCells xs
+  | otherwise = (x1, c1) : addNeededEmptyCells ((x2, c2) : xs)
+
+-- Funkcja wstawiająca elementy z listy we wszystkich możliwych miejscach
+--
+-- source: https://stackoverflow.com/questions/31288206/how-to-insert-a-list-in-a-list-in-all-possible-ways
+interleave :: [a] -> [a] -> [[a]]
+interleave xs [] = [xs]
+interleave [] ys = [ys]
+interleave xs@(x:xs') ys@(y:ys') = map (x :) (interleave xs' ys) ++ map (y :) (interleave xs ys')
+
+-- Funkcja grupująca wszystkie występujące po sobie bloki tego samego koloru w jeden blok
+groupBlocks :: [Block] -> [Block]
+groupBlocks [] = []
+groupBlocks [x] = [x]
+groupBlocks ((x1, c1):(x2, c2):xs)
+  | c1 == c2 = groupBlocks ((x1 + x2, c1) : xs)
+  | otherwise = (x1, c1) : groupBlocks ((x2, c2) : xs)
+
+-- Funkcja generująca n-elementową listę tych samych wartości
+generateN :: a -> Int -> [a]
+generateN a n = [a | _ <- [1 .. n]]
+
+-- Funkcja generują wszystkie możliwe rozwiązania wiersza/kolumny
+--
+-- 1st arg: [Block] - warunki wiersza/kolumny
+-- 2nd arg: Int -> ilosc komórek w wierszu/kolumnie
+allPossibleSolutions :: [Block] -> Int -> [[Block]]
+allPossibleSolutions bs n =
+  let filledWithNeededEmpty = addNeededEmptyCells bs
+      remainingEmptyCells = countEmpty filledWithNeededEmpty n
+      remainingEmptyBlocks = generateN (1, Empty) remainingEmptyCells
+   -- nub - usuwa duplikaty z listy
+   -- (mogą wystąpić przy generowaniu rozwiązań gdy sąsiednie bloki są tego samego koloru)
+   in nub (map groupBlocks (interleave remainingEmptyBlocks filledWithNeededEmpty))
---
+++ test/Algorithms/AllSolutionsSpec.hs
@@ -0,0 +1,64 @@
+module Algorithms.AllSolutionsSpec (spec) where
+
+import Test.Hspec
+import Algorithms
+import Model
+
+spec :: Spec
+spec = do
+  describe "countEmpty" $ do
+    it "should return 0 if no empty cells should be in row/cell" $ do
+      let constraints = [(1, Black), (2, Orange)]
+      let n = 3
+      countEmpty constraints n `shouldBe` 0
+
+    it "counts how many empty cells should be in row/cell" $ do
+      let constraints = [(1, Black), (1, Orange)]
+      let n = 5
+      countEmpty constraints n `shouldBe` 3
+
+      let constraints2 = [(1, Black), (2, Orange)]
+      countEmpty constraints2 n `shouldBe` 2
+
+  describe "addNeededEmptyCells" $ do
+    it "should not add any empty cell if not needed" $ do
+      let constraints = [(1, Black), (2, Orange)]
+      addNeededEmptyCells constraints `shouldBe` constraints
+
+    it "should add empty cells if needed (1 cell)" $ do
+      let constraints = [(1, Black), (2, Black)]
+      addNeededEmptyCells constraints `shouldBe` [(1, Black), (1, Empty), (2, Black)]
+
+    it "should add empty cells if needed (2 cells)" $ do
+      let constraints = [(1, Black), (2, Black), (1, Orange), (2, Orange)]
+
+      addNeededEmptyCells constraints
+        `shouldBe` [(1, Black), (1, Empty), (2, Black), (1, Orange), (1, Empty), (2, Orange)]
+
+  describe "groupBlocks" $ do
+    it "should not group blocks with different colors" $ do
+      let blocks = [(1, Black), (2, Orange), (3, Empty)]
+      groupBlocks blocks `shouldBe` blocks
+
+    it "should group blocks with the same colors" $ do
+      let blocks = [(1, Black), (2, Black), (2, Orange), (1, Empty), (1, Empty), (1, Empty), (3, Red)]
+      groupBlocks blocks `shouldBe` [(3, Black), (2, Orange), (3, Empty), (3, Red)]
+
+  describe "generateN" $ do
+    it "generates n-size list with all the same elements" $ do
+      generateN (1, Empty) 4 `shouldBe` [(1, Empty), (1, Empty), (1, Empty), (1, Empty)]
+
+  describe "allPossibleSolutions" $ do
+    it "generates all possible solutions of the row/column (different colors)" $ do
+      let constraints = [(1, Black), (2, Orange)]
+      let size = 5
+      let solutions = allPossibleSolutions constraints size
+
+      length solutions `shouldBe` 6
+
+    it "generates all possible solutions of the row/column (with the same colors)" $ do
+      let constraints = [(2, Black), (2, Black)]
+      let size = 6
+
+      length (allPossibleSolutions constraints size) `shouldBe` 3
+
