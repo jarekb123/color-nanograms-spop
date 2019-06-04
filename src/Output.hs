dc4dc1654d26f29cf81db7b3bd5b58b0337b00a8
Pawel Wasowski <pwasows@gmail.com>
2019-06-04 20:44:35 +0200

    Add Output module, implementing outputNanogram function

---
+++ src/Output.hs
@@ -0,0 +1,33 @@
+module Output where
+
+import qualified System.Console.ANSI as Console
+import Model
+
+translateColor :: Color -> Console.Color
+translateColor color
+    | color == Empty = Console.White
+    | color == Black = Console.Black
+    | color == Red = Console.Red
+    | color == Green = Console.Green
+    | color == Yellow = Console.Yellow
+    | color == Orange = Console.Magenta -- brak pomarańczowego w System.Console.Ansi
+
+testNanogram = [[Red, Black, Red],
+                [Red, Black, Red],
+                [Black, Red, Black]]
+
+putCharWithColor :: Char -> Console.Color -> IO ()
+putCharWithColor x c = do
+    Console.setSGR [Console.SetColor Console.Foreground Console.Vivid c]
+    putChar x
+    Console.setSGR [Console.Reset]
+
+outputRow :: [Color] -> IO ()
+outputRow [] = putChar '\n'
+outputRow (x:xs) = do   (putCharWithColor 'X') (translateColor x)
+                        outputRow xs
+
+outputNanogram :: Nanogram -> IO ()
+outputNanogram [] = putChar '\n'
+outputNanogram (row:rows) = do outputRow row
+                               outputNanogram rows
